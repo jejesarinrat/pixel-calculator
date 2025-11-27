4279d2da291da90e7cddfca05061fe64d374b879
jejesarinrat <jejesarinrat@gmail.com>
2025-11-27 19:02:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const display = document.getElementById('display');
+const historyDisplay = document.getElementById('history');
+
+function appendToDisplay(input) {
+    if(display.value === "Error" || display.value === "GAME OVER") {
+        display.value = "";
+        historyDisplay.innerText = "";
+    }
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+    historyDisplay.innerText = "";
+}
+
+function deleteLastChar() {
+    if(display.value === "Error" || display.value === "GAME OVER") {
+        display.value = "";
+        historyDisplay.innerText = "";
+    } else {
+        display.value = display.value.toString().slice(0, -1);
+    }
+}
+
+function calculate() {
+    try {
+        let expression = display.value;
+        if(expression === "") return;
+
+        let result = eval(expression);
+
+        if (!isFinite(result) || isNaN(result)) {
+            throw new Error("Division by Zero");
+        }
+
+        historyDisplay.innerText = expression + " ="; 
+        display.value = result;
+
+    } catch (error) {
+        display.value = "GAME OVER";
+        historyDisplay.innerText = "";
+    }
+}
